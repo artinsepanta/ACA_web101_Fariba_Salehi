4fad14f2633014c60a517e8213e16fcc1067ff01
artinsepanta <faribasalehi@hotmail.com>
2019-02-27 15:36:08 -0600

    tictactoe

---
+++ DOM-ticTacTOE/scripts/ticTacToe.js
@@ -0,0 +1,18 @@
+var gameMarker=document.getElementsById("x").innerHTML;
+
+function markSquare(){
+  
+  document.getElementsByClassName("square")[0 ,1,2,3,4,5,6,7,8].innerHTML="x";
+}
+function  changeMarkerToX(){
+  var gameMarker = "X"
+  console.log("The x button was clicked!");
+}
+function  changeMarkerO(){
+  var gameMarker = "0"
+  console.log("The x button was clicked!");
+}
+
+function placeMark(){ 
+  var whenEachSquareIsClicked=document.getElementsById("").innerHTML,
+}
