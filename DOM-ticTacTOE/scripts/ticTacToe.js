e6f5aa3c2d2083e82291d50286f0acabdee26af6
artinsepanta <faribasalehi@hotmail.com>
2019-03-02 00:02:35 -0600

    more learning js

--- DOM-ticTacTOE/scripts/ticTacToe.js
+++ DOM-ticTacTOE/scripts/ticTacToe.js
@@ -1,8 +1,31 @@
-var gameMarker=document.getElementsById("x").innerHTML;
 
+  function startGame(){
+    document.trun="X" ;
+
+    setMessage(document.turn + " gets to start.");
+
+  }
+  function setMessage(msg){
+    document.getElementById("message").innerText=msg;
+  }
+  function nextMove(square){
+   square.innerText=document.turn;
+   switchTurn();
+  }
+  function switchTurn(){
+    if (document.turn=="X"){
+        document.turn="O";
+    } else{
+         document.turn="X";
+    }
+       setMessage("It's"= document.turn+ " 's turn!");
+  }
+
+var gameMarker=document.getElementsById("x").innerHTML;
+ 
 function markSquare(){
   
-  document.getElementsByClassName("square")[0 ,1,2,3,4,5,6,7,8].innerHTML="x";
+  document.getElementsByClassName("square")[0 ,1,2,3,4,5,6,7,8.].innerHTML="x";
 }
 function  changeMarkerToX(){
   var gameMarker = "X"
@@ -16,3 +39,4 @@ function  changeMarkerO(){
 function placeMark(){ 
   var whenEachSquareIsClicked=document.getElementsById("").innerHTML,
 }
+
